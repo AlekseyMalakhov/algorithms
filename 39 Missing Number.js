ab4e779b369e0945ce4ca6f6b4b18ef5f0a1cb88
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-03-20 16:15:41 +0300

    add missing number

---
+++ 39 Missing Number.js
@@ -0,0 +1,56 @@
+/*
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 104
+    0 <= nums[i] <= n
+    All the numbers of nums are unique.
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+*/
+
+var missingNumber = function (nums) {
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  if (nums[0] !== 0) {
+    return 0;
+  }
+  if (nums[n - 1] !== n) {
+    return n;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (i !== nums[i]) {
+      return i;
+    }
+  }
+};
+
+// console.log(missingNumber([3, 0, 1]));
+// console.log(missingNumber([0, 1]));
+// console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
+const c1 = [
+  45, 35, 38, 13, 12, 23, 48, 15, 44, 21, 43, 26, 6, 37, 1, 19, 22, 3, 11, 32,
+  4, 16, 28, 49, 29, 36, 33, 8, 9, 39, 46, 17, 41, 7, 2, 5, 27, 20, 40, 34, 30,
+  25, 47, 0, 31, 42, 24, 10, 14,
+];
+console.log(missingNumber(c1)); //18
