23312b0c35cee5bc681c2a4da22af06db6be82cf
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-04-03 15:06:54 +0300

    add another solution

--- 85 Longest Harmonious Subsequence.js
+++ 85 Longest Harmonious Subsequence.js
@@ -23,74 +23,100 @@ Constraints:
 */
 
 var findLHS = function (nums) {
-    let length = 0;
-    let max = 0;
-    let min = 0;
-    let lengthMax = 0;
-    let lengthMin = 0;
+    const obj = {};
+
     for (let i = 0; i < nums.length; i++) {
-        const check1 = nums[i];
-        //console.log("check1 = " + check1);
-        max = check1 + 1;
-        min = check1 - 1;
-        let diff = false;
-        for (let j = i + 1; j < nums.length; j++) {
-            const check2 = nums[j];
-            if (check2 === max) {
-                diff = true;
-                if (lengthMax === 0) {
-                    //console.log("check2 = " + check2 + ", j = " + j + ", to max");
-                    lengthMax++;
-                }
-                //console.log("check2 = " + check2 + ", j = " + j + ", to max");
-                lengthMax++;
-            }
-            if (check2 === min) {
-                diff = true;
-                if (lengthMin === 0) {
-                    lengthMin++;
-                }
-                lengthMin++;
-            }
-            if (check2 === check1) {
-                if (lengthMin === 0) {
-                    //console.log("check2 = " + check2 + ", j = " + j);
-                    lengthMin++;
-                }
-                if (lengthMax === 0) {
-                    //console.log("check2 = " + check2 + ", j = " + j + ", to max");
-                    lengthMax++;
-                }
-                //console.log("check2 = " + check2 + ", j = " + j + ", to max");
-                lengthMin++;
-                lengthMax++;
-            }
+        const check = nums[i];
+        if (!obj[check]) {
+            obj[check] = {
+                valMin: 1,
+                valMax: 1,
+                real: true,
+            };
+        } else {
+            obj[check].valMin++;
+            obj[check].valMax++;
+            obj[check].real = true;
         }
-        const temp = Math.max(lengthMax, lengthMin);
-        // console.log("max = " + max);
-        // console.log("min = " + min);
-        // console.log("lengthMax = " + lengthMax);
-        // console.log("lengthMin = " + lengthMin);
-        //console.log("---------------------");
-        if (temp > length && diff) {
-            length = temp;
+
+        if (!obj[check - 1]) {
+            obj[check - 1] = {
+                valMin: 0,
+                valMax: 1,
+                real: false,
+            };
+        } else {
+            obj[check - 1].valMax++;
         }
-        lengthMax = 0;
-        lengthMin = 0;
+
+        if (!obj[check + 1]) {
+            obj[check + 1] = {
+                valMin: 1,
+                valMax: 0,
+                real: false,
+            };
+        } else {
+            obj[check + 1].valMin++;
+        }
     }
-    return length;
+
+    console.log(obj);
+
+    let min = 0;
+    let max = 0;
+    for (x in obj) {
+        const check = obj[x];
+        if (check.real && check.valMin !== check.valMax) {
+            if (check.valMin < min) {
+                min = check.valMin;
+            }
+            if (check.valMax > max) {
+                max = check.valMax;
+            }
+        }
+    }
+
+    return Math.max(min, max);
 };
 
-console.log(findLHS([1, 3, 2, 2, 5, 2, 3, 7])); //5
-console.log(findLHS([1, 2, 3, 4])); //2
-console.log(findLHS([1, 1, 1, 1])); //0
+var findLHS1 = function (nums) {
+    let maxCount = 0;
 
-console.log(findLHS([1, 4, 1, 3, 1, -14, 1, -13])); //2
+    let numsMap = new Map();
 
-console.log(findLHS([1, 2, 1, 3, 0, 0, 2, 2, 1, 3, 3])); //6
+    for (const num of nums) {
+        if (numsMap.get(num)) {
+            numsMap.set(num, numsMap.get(num) + 1);
+        } else {
+            numsMap.set(num, 1);
+        }
+    }
 
-console.log(findLHS([-3, -1, -1, -1, -3, -2])); //4
+    console.log(numsMap);
+    for (const [key, value] of numsMap) {
+        if (numsMap.get(key + 1)) {
+            const sum = value + numsMap.get(key + 1);
 
-console.log(findLHS([2, 2, 2, 2, 2, 2, 2, 3, 1, 0, 0, 0, 3, 1, -1, 0, 1, 1, 0, 0, 1, 1, 2, 2, 2, 0, 1, 2, 2, 3, 2])); //20
+            maxCount = sum > maxCount ? sum : maxCount;
+        }
+    }
+
+    return maxCount;
+};
+
+//console.log(findLHS([1, 3, 2, 2, 5, 2, 3, 7])); //5
+console.log("---------------");
+//console.log(findLHS1([1, 3, 2, 2, 5, 2, 3, 7])); //5
+//console.log(findLHS([1, 2, 3, 4])); //2
+//console.log(findLHS([1, 1, 1, 1])); //0
+
+//console.log(findLHS([1, 4, 1, 3, 1, -14, 1, -13])); //2
+
+//console.log(findLHS([1, 2, 1, 3, 0, 0, 2, 2, 1, 3, 3])); //6
+
+//console.log(findLHS([-3, -1, -1, -1, -3, -2])); //4
+
+//console.log(findLHS([2, 2, 2, 2, 2, 2, 2, 3, 1, 0, 0, 0, 3, 1, -1, 0, 1, 1, 0, 0, 1, 1, 2, 2, 2, 0, 1, 2, 2, 3, 2])); //20
 
 console.log(findLHS([2, 3, 1, 1, 1])); //4
+console.log(findLHS1([2, 3, 1, 1, 1])); //4
