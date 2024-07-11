a05299cee596cce32a13b7ff35bb050d9cee80d1
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-07-11 17:47:44 +0300

    and finally it works

--- 269 Find if Path Exists in Graph.js
+++ 269 Find if Path Exists in Graph.js
@@ -28,156 +28,8 @@ Constraints:
     There are no duplicate edges.
     There are no self edges.
 */
-let n = 0;
-const checkCalls = () => {
-    n++;
-    console.log("Recursive function call = " + n);
-};
 
 var validPath = function (n, edges, source, destination) {
-    //console.log("start");
-    //check edge case
-    if (edges.length === 0 && source === 0 && destination === 0) {
-        return true;
-    }
-
-    //let's create a list of directions for every point
-    const routesForPoint = [];
-    const edgesWithBackRoutes = [];
-
-    for (const val of edges) {
-        const valReversed = [val[1], val[0]];
-        edgesWithBackRoutes.push(val);
-        edgesWithBackRoutes.push(valReversed);
-
-        //and at the same time check if there is a direct route
-        if ((val[1] === source && val[0] === destination) || (val[0] === source && val[1] === destination)) {
-            return true;
-        }
-
-        const p1 = val[0];
-        const p2 = val[1];
-        if (routesForPoint[p1] === undefined) {
-            routesForPoint[p1] = [];
-        }
-        if (routesForPoint[p2] === undefined) {
-            routesForPoint[p2] = [];
-        }
-        routesForPoint[p1].push(val);
-        routesForPoint[p2].push(valReversed);
-    }
-
-    // // console.log("edgesWithBackRoutes:", edgesWithBackRoutes);
-
-    // // console.log("edgesWithBackRoutes ready");
-
-    // // console.log("routesForPoint:", routesForPoint);
-    // // console.log("routesForPoint ready");
-    /*
-    routesForPoint: [
-        [ [ 0, 1 ], [ 0, 2 ], [ 0, 3 ] ],
-        [ [ 1, 0 ] ],
-        [ [ 2, 0 ], [ 2, 4 ] ],
-        [ [ 3, 0 ] ],
-        [ [ 4, 2 ] ]
-      ]
-        */
-
-    //create a stack for routes
-    const path = [];
-
-    //create an object to easily check if route is used
-    const used = new Map();
-
-    //when we put route into stack we will at the same time put it into used object
-
-    let record = 5;
-
-    const checkRoutes = (prevPoint, point) => {
-        //checkCalls();
-        // console.log("--------------------New checkRoutes call------------------------");
-        //debug
-        // if (path.length > record) {
-        //     record = record * 2;
-        // //     // console.log("record = " + record);
-        // }
-        //end debug
-        //console.log("point:", point);
-        const routes = routesForPoint[point];
-        for (const route of routes) {
-            // console.log("point: " + point + " - " + "route:", route);
-            //console.log("used:", used);
-            const isUsed = used.has(route);
-            // console.log("point: " + point + " - " + "isUsed:", isUsed);
-            if (!isUsed && route[1] !== prevPoint) {
-                //if this route does not lead back to the previous point
-                //add this route to the path
-                // console.log("point: " + point + " - " + "good route found " + route);
-                path.push(route);
-                used.set(route, true);
-                //create new point to go
-                const newPoint = route[1];
-                //update previous point
-                const newPrevPoint = point;
-                // console.log("point: " + point + " - " + "prevPoint:", newPrevPoint);
-                // console.log("point: " + point + " - " + "newPoint:", newPoint);
-                if (newPoint === destination) {
-                    // console.log("point: " + point + " - " + "-------------this is destination!------------");
-                    return true;
-                } else {
-                    // console.log("point: " + point + " - " + "Run new checkRoutes with prevPoint = " + newPrevPoint + ", and point = " + newPoint);
-
-                    //normal solution
-                    const res = checkRoutes(newPrevPoint, newPoint);
-                    // console.log("point: " + point + " - " + "Finish checkRoutes with prevPoint = " + newPrevPoint + ", and point = " + newPoint);
-                    if (res) {
-                        return true;
-                    } else {
-                        //this route we recently added is wrong. Remove it from stack
-                        const wrongPath = path.pop();
-                        // console.log("point: " + point + " - " + "wrongPath --------------:", wrongPath);
-                    }
-                }
-            }
-        }
-        //if we checked all routes but have not found correct one, probably
-        //this route is wrong
-        // console.log("point: " + point + " - " + "this rout is wrong");
-        return false;
-    };
-
-    const res = checkRoutes(null, source);
-    // // console.log(path);
-    //console.log("lala");
-    return res;
-};
-
-var validPath2 = function (n, edges, source, destination) {
-    let g = new Array(n);
-    for (let i = 0; i < n; i++) g[i] = [];
-    for (let i of edges) {
-        g[i[0]].push(i[1]);
-        g[i[1]].push(i[0]);
-    }
-    //console.log("g:", g); //this is the same as my routesForPoint
-    // [ [ 1, 2, 3 ], [ 0 ], [ 0, 4 ], [ 0 ], [ 2 ] ]
-
-    let vis = new Array(n).fill(0); //handle visited
-
-    rec(source, g, vis);
-    return vis[destination];
-};
-
-var rec = (node, g, vis) => {
-    //checkCalls();
-    vis[node] = 1; //mark the node as visited. Similar as my used.set(route, true);
-    for (let i of g[node]) {
-        //iterate along the possible routes for the current node. Similar to my for (const route of routes) {
-        if (!vis[i]) rec(i, g, vis); //similar to mine  if (!isUsed && route[1] !== prevPoint) { -
-    }
-};
-
-var validPath3 = function (n, edges, source, destination) {
     if (edges.length === 0 && source === 0 && destination === 0) {
         return true;
     }
@@ -205,16 +57,17 @@ var validPath3 = function (n, edges, source, destination) {
         routesForPoint[p2].push(valReversed);
     }
 
-    const used = new Map();
+    const used = [];
 
     const checkRoutes = (prevPoint, point) => {
         const routes = routesForPoint[point];
         for (const route of routes) {
-            const isUsed = used.has(route);
+            const direction = route[1];
+            const isUsed = used[direction];
 
-            if (!isUsed && route[1] !== prevPoint) {
-                used.set(route, true);
-                const newPoint = route[1];
+            if (!isUsed && direction !== prevPoint) {
+                used[direction] = true;
+                const newPoint = direction;
                 const newPrevPoint = point;
                 if (newPoint === destination) {
                     result = true;
@@ -297,20 +150,8 @@ import { edges } from "./edges2.js";
 
 const start = performance.now();
 
-console.log(validPath3(2498, edges, 0, 2497));
+console.log(validPath(2498, edges, 0, 2497));
 
 const end = performance.now();
 
 console.log(`Execution time: ${end - start} ms`);
-
-/*
-validPath and validPath2 has the same amount of recursion calls
-and when calling with console.log they differ only for 4 ms, despite
-their execution time increases to 102 and 106 ms
-it means that problem probably not in recursion itself but in preparation 
-phase
-
-lets try to remove recursion and leave only the preparation phase
-Preparation phase is very similar in time - 6 and 7 ms
-
-*/
