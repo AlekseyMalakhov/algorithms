7ffb65ad3a28a628341beec924f6b90fd2d51b44
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2026-04-16 10:57:01 +0300

    done

--- 284 DI String Match.ts
+++ 284 DI String Match.ts
@@ -1,34 +1,34 @@
-/*
-A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
+const diStringMatch = (s: string): number[] => {
+    const arr: number[] = [];
+    const sArr = s.split("");
 
-    s[i] == 'I' if perm[i] < perm[i + 1], and
-    s[i] == 'D' if perm[i] > perm[i + 1].
+    let iN = 0;
+    sArr.forEach((item) => {
+        if (item === "I") {
+            iN++;
+        }
+    });
 
-Given a string s, reconstruct the permutation perm and return it. If there are multiple valid permutations perm, return any of them.
+    const start = s.length - iN;
+    let high = start + 1;
+    let low = start - 1;
 
- 
+    for (let i = 0; i < sArr.length; i++) {
+        if (i === 0) {
+            arr.push(start);
+        }
+        if (s[i] === "I") {
+            arr.push(high);
+            high++;
+        } else {
+            arr.push(low);
+            low--;
+        }
+    }
 
-Example 1:
+    return arr;
+};
 
-Input: s = "IDID"
-Output: [0,4,1,3,2]
-
-Example 2:
-
-Input: s = "III"
-Output: [0,1,2,3]
-
-Example 3:
-
-Input: s = "DDI"
-Output: [3,2,0,1]
-
- 
-
-Constraints:
-
-    1 <= s.length <= 105
-    s[i] is either 'I' or 'D'.
-
-*/
-
+console.log(diStringMatch("IDID"));
+console.log(diStringMatch("III"));
+console.log(diStringMatch("DDI"));
